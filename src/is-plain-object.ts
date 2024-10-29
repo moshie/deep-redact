d478db762c87a03364c279f3d05c7dc77fa361bb
David Hewitt <hello@wp-unite.io>
2024-10-29 09:08:52 +0000

    add type guard to is plain object

--- src/is-plain-object.ts
+++ src/is-plain-object.ts
@@ -1,5 +1,6 @@
-export const isPlainObject = (obj: object) => (
-    obj !== null &&
-    typeof obj === 'object' &&
-    [null, Object.prototype].includes(Object.getPrototypeOf(obj))
-);
+import type { Data, DataTypes } from "./types";
+
+export const isPlainObject = (obj: Data): obj is Record<string, DataTypes> =>
+	obj !== null &&
+	typeof obj === "object" &&
+	[null, Object.prototype].includes(Object.getPrototypeOf(obj));
