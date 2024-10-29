e7ab19a3743b6f580911b925f4556219ffe9ecda
David Hewitt <hello@wp-unite.io>
2024-10-29 09:09:34 +0000

    update data types

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,25 @@
+export type DataTypes =
+	| string
+	| object
+	| symbol
+	| boolean
+	| bigint
+	| undefined
+	| null
+	| number;
+
+export type Data = DataTypes[] | DataTypes;
+
 export type List = string[];
 
 export type RedactOptions = {
-    list?: List;
-    redactString?: string;
-}
+	list?: List;
+	strict?: boolean;
+	redactString?: string;
+};
 
 export type CanBeRedactedArgs = {
-    key: string;
-    value: any;
-    list: List;
-}
+	key: string;
+	value: DataTypes;
+	list: List;
+};
