a16f7be9cdf3934fac218104dda3054218644653
github-action-benchmark <github@users.noreply.github.com>
2025-06-14 10:47:15 +0000

    add Deep Redact JS Benchmark (benchmarkjs) benchmark result for 6d3cf7ffa355f56ca330f973ef996144cae644d1

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1749898035064,
+  "repoUrl": "https://github.com/moshie/deep-redact",
+  "entries": {
+    "Deep Redact JS Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "moshie@users.noreply.github.com",
+            "name": "David Hewitt",
+            "username": "moshie"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6d3cf7ffa355f56ca330f973ef996144cae644d1",
+          "message": "Merge pull request #32 from moshie/remove-external-data-json-path\n\nremove property from gha",
+          "timestamp": "2025-06-14T11:46:41+01:00",
+          "tree_id": "2fc2b0a17bebe5756b453c74e8d662319b8ab9ec",
+          "url": "https://github.com/moshie/deep-redact/commit/6d3cf7ffa355f56ca330f973ef996144cae644d1"
+        },
+        "date": 1749898034159,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Redact#redact",
+            "value": 9176,
+            "range": "±1.01%",
+            "unit": "ops/sec",
+            "extra": "87 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
